bc14532027d19450e9680c964d4bd36162600d92
prakashcr <prakash8296@gmail.com>
2017-08-17 11:38:16 +0530

    backend
    commit 4

--- src/main/java/com/niit/model/Product.java
+++ src/main/java/com/niit/model/Product.java
@@ -4,6 +4,9 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Transient;
+
+import org.springframework.web.multipart.MultipartFile;
 
 @Entity
 public class Product {
@@ -13,8 +16,21 @@ public class Product {
 	private String pname;
 	private String stock;
 	private String price;
-    private String brandname;
-	public int getPid() {
+	private String brandname;
+	@Transient
+	private MultipartFile image;
+
+	public MultipartFile getImage() {
+		return image;
+	}
+
+	public void setImage(MultipartFile image) {
+		this.image = image;
+	}
+
+	public int getPid()
+
+	{
 		return pid;
 	}
 
